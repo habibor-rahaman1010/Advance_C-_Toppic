b6d5924af52b9244815006fb461597f77f43ada2
habibor-rahaman1010 <habibor144369@gmail.com>
2024-02-08 09:56:03 +0600

    ok

---
+++ Advanced_C#/DelegatesExample/Delegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    public class Delegate
+    {
+        public delegate void Perform(string text);
+
+        public void PrintingMethod(string textToPrint)
+        {
+            Console.WriteLine($"--::{textToPrint}::--");
+        }
+
+        public void ProcessText(string text, Perform perform)
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                perform.Invoke(text);
+            }
+        }
+    }
+}
---
+++ Advanced_C#/DelegatesExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DelegatesExample
+{
+    public class Program
+    {
+        public delegate void SMS(string sms);
+
+        static void Main(string[] args)
+        {
+            Delegate myDelegate = new Delegate();
+            string text = "Hello Habibor";
+            myDelegate.ProcessText(text, myDelegate.PrintingMethod);
+
+
+            //for second delegates
+            //SMS logics = PrintingSMS; //same as SMS logics = new SMS(PrintingSMS);
+            //SMS logics = myDelegate.PrintingMethod; // myDelegate.PrintingMethod this method signature same as SMS delegates that so we can put this method in SMS delegate
+            SMS logics = new SMS(PrintingSMS);
+            string sms = "This is my text from c# batch";
+            ProcesSMS(sms, logics);
+        }
+
+        public static void PrintingSMS(string sms)
+        {
+            Console.WriteLine($"--{sms}--");
+        }
+
+        public static void ProcesSMS(string sms, SMS sendSms)
+        {
+            if (!string.IsNullOrWhiteSpace(sms))
+            {
+                sendSms.Invoke(sms);
+            }
+        }
+    }
+}
